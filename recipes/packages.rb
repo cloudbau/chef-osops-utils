c34d73980ba135c21d3076e49f2a28431700208e
Hendrik Volkmer <h.volkmer@cloudbau.de>
2012-11-30 15:40:13 +0100

    Add folsom packages

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -65,16 +65,26 @@ when "ubuntu","debian"
     notifies :run, resources(:execute => "apt-get update"), :immediately
   end
 
+  # file "/var/lib/apt/periodic/update-success-stamp" do
+  #   action :delete
+  # end
+
+  package 'ubuntu-cloud-keyring'
+
   apt_repository "openstack-folsom" do
-    #uri "http://ubuntu-cloud.archive.canonical.com/ubuntu"
-    #distribution "precise-updates/folsom"
-    uri "http://ppa.launchpad.net/openstack-ubuntu-testing/folsom-trunk-testing/ubuntu"
-    distribution node["lsb"]["codename"]
+    uri "http://ubuntu-cloud.archive.canonical.com/ubuntu"
+    distribution "precise-updates/folsom"
+    #uri "http://ppa.launchpad.net/openstack-ubuntu-testing/folsom-trunk-testing/ubuntu"
+    #distribution node["lsb"]["codename"]
     components ["main"]
-    keyserver "keyserver.ubuntu.com"
-    key "3B6F61A6"
+    #keyserver "keyserver.ubuntu.com"
+    #key "3B6F61A6"
     notifies :run, resources(:execute => "apt-get update"), :immediately
  end
+
+ execute "enforced upt-get update" do
+    command "apt-get update"
+  end
 end
 
 
